b95e81574340e97126b7ca92897008e0566b5b73
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-02-08 15:47:26 -0500

    replace disasm

--- build.bat
+++ build.bat
@@ -1,18 +1,4 @@
 @echo off
+asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- S2210892.asm, s2built.bin
 
-echo Build Started
-
-IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
-
-asm68k /k /p /q /o ae- Sonic2Alpha.asm, s2built.bin, , list.lst > log.txt
-IF NOT EXIST s2built.bin goto LABLERR
-
-echo Build Successful!
-
-goto LABLDONE
-
-:LABLERR
-type log.txt
-echo There was a problem building. Please check through the error message and fix the error.
-:LABLDONE
 pause
